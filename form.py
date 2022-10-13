f755ece676507c019feb6795b903ee960b915324
Arpan Pal <arpanpalme@gmail.com>
2022-10-13 12:45:56 +0530

    adding login/registration form, all blog page

---
+++ form.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, PasswordField, StringField, EmailField
+from wtforms.validators import DataRequired
+from flask_ckeditor import CKEditorField
+
+
+class LoginForm(FlaskForm):
+    email = EmailField(label="Enter your email", validators=[DataRequired()])
+    password = PasswordField(label="Enter your password", validators=[DataRequired()])
+
+
+class Registration(FlaskForm):
+    name = StringField(label="Enter your full name", validators=[DataRequired()])
+    email = EmailField(label="Enter your email", validators=[DataRequired()])
+    password = PasswordField(label="Enter your password", validators=[DataRequired()])
+    c_password = PasswordField(label="Confirm your password", validators=[DataRequired()])
+
+
+class PostBlog(FlaskForm):
+    title = StringField(label="Enter the Blog title", validators=[DataRequired()])
+    subtitle = StringField(label="Enter the sub-title", validators=[DataRequired()])
+    # author = StringField(label="name")
+    body = CKEditorField('body', validators=[DataRequired()])
--- main.py
+++ main.py
@@ -1,11 +1,61 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from form import LoginForm, Registration, PostBlog
+from flask_ckeditor import CKEditor
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'any secret string'
+
+bootstrap = Bootstrap(app)
+ckeditor = CKEditor(app)
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    form = LoginForm()
+    if request.method == "POST":
+        # if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        print(email)
+        print(password)
+        return f"{email} {password}"
+    return render_template("login.html", form=form)
+
+
+@app.route("/registration", methods=["POST", "GET"])
+def registration():
+    form = Registration()
+    if request.method == "POST":
+        name = form.name.data
+        email = form.email.data
+        password = form.password.data
+        c_password = form.c_password.data
+        return f"{name} {email} {password} {c_password}"
+    return render_template("registration.html", form=form)
+
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+
+@app.route("/blog/<int:id>")
+def blog_details(id):
+    return render_template("blog.html")
+
+
+@app.route("/post")
+def post():
+    form = PostBlog()
+    return render_template("post.html", form=form)
+
 
 
 if __name__ == '__main__':
