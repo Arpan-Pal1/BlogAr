fe08edc7ea7ec3f740258d07be0d24486e60dee3
Arpan Pal <arpanpalme@gmail.com>
2022-10-15 10:11:13 +0530

    adding user table, creating login/reg function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from form import LoginForm, Registration, PostBlog
 from flask_ckeditor import CKEditor
 from flask_bootstrap import Bootstrap
@@ -17,6 +19,13 @@ bootstrap = Bootstrap(app)
 ckeditor = CKEditor(app)
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 
 class Blog(db.Model):
     id = Column(Integer, primary_key=True)
@@ -24,6 +33,13 @@ class Blog(db.Model):
     subtitle = Column(String(250))
     date = Column(String(20))
     body = Column(String(250))
+
+
+class User(db.Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    email = Column(String(50))
+    password = Column(String(50))
 
 
 db.create_all()
@@ -38,12 +54,18 @@ def home():
 def login():
     form = LoginForm()
     if request.method == "POST":
-        # if form.validate_on_submit():
-        email = form.email.data
-        password = form.password.data
-        print(email)
-        print(password)
-        return f"{email} {password}"
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password,password):
+                return redirect('/blog')
+            else:
+                flash("Invalid credential")
+                return redirect('/login')
+        else:
+            flash("This email is not registered yet. Register first")
+            return redirect('/registration')
     return render_template("login.html", form=form)
 
 
@@ -53,16 +75,32 @@ def registration():
     if request.method == "POST":
         name = form.name.data
         email = form.email.data
-        password = form.password.data
-        c_password = form.c_password.data
-        return f"{name} {email} {password} {c_password}"
-    return render_template("registration.html", form=form)
+        # print(email)
+        # email_check = User.query.filter_by(email=email).first()
+        if not User.query.filter_by(email=email).first():
+            password = form.password.data
+            c_password = form.c_password.data
+            if password == c_password:
+                hashed_password = generate_password_hash(password=password, method="pbkdf2:sha256", salt_length=8)
+                new_user = User(name=name, email=email, password=hashed_password)
+                db.session.add(new_user)
+                db.session.commit()
+                load_user(new_user.id)
+                return redirect('/blog')
+            else:
+                flash("please enter password correctly")
+                return redirect(url_for('registration'))
+        else:
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
+
+    return render_template("registration.html", form=form, logged_in=current_user.is_authenticated)
 
 
 @app.route("/blog")
 def blog():
     all_blog = db.session.query(Blog).all()
-    return render_template("blog.html", blogs=all_blog)
+    return render_template("blog.html", blogs=all_blog, name=current_user, logged_in=True)
 
 
 @app.route("/blog/<int:id>")
@@ -87,9 +125,22 @@ def post():
     return render_template("post.html", form=form)
 
 
-@app.route("/update/<int:id>")
+@app.route("/update/<int:id>", methods=["POST", "GET"])
 def update(id):
-    pass
+    blog_post = Blog.query.get(id)
+    print(blog_post)
+    blog_record = PostBlog(
+        title=blog_post.title,
+        subtitle=blog_post.subtitle,
+        body=blog_post.body
+    )
+    if blog_record.validate_on_submit():
+        blog_post.title = blog_record.title.data
+        blog_post.subtitle = blog_record.subtitle.data
+        blog_post.body = blog_record.body.data
+        db.session.commit()
+        return redirect("/blog")
+    return render_template("update.html", form=blog_record)
 
 
 @app.route("/delete/<int:id>")
@@ -98,7 +149,6 @@ def delete(id):
     db.session.delete(blog_to_delete)
     db.session.commit()
     return redirect("/blog")
-
 
 
 if __name__ == '__main__':
