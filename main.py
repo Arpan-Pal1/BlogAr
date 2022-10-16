79c8e9ecd07a72c1ea216af4b19d9e9d6d713438
Arpan Pal <arpanpalme@gmail.com>
2022-10-16 08:39:51 +0530

    user authentication and restrictions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect, flash, url_for
+from flask import Flask, render_template, request, redirect, flash, url_for, abort
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from functools import wraps
+
 from form import LoginForm, Registration, PostBlog
 from flask_ckeditor import CKEditor
 from flask_bootstrap import Bootstrap
@@ -26,8 +28,16 @@ login_manager.init_app(app)
 def load_user(user_id):
     return User.query.get(user_id)
 
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if current_user.id != 1:
+            return abort(403)
+        return f(*args, **kwargs)
+    return decorated_function
 
-class Blog(db.Model):
+
+class Blog(db.Model, UserMixin):
     id = Column(Integer, primary_key=True)
     title = Column(String(250))
     subtitle = Column(String(250))
@@ -35,7 +45,7 @@ class Blog(db.Model):
     body = Column(String(250))
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
     email = Column(String(50))
@@ -58,7 +68,8 @@ def login():
         password = request.form.get('password')
         user = User.query.filter_by(email=email).first()
         if user:
-            if check_password_hash(user.password,password):
+            if check_password_hash(user.password, password):
+                login_user(user)
                 return redirect('/blog')
             else:
                 flash("Invalid credential")
@@ -66,7 +77,13 @@ def login():
         else:
             flash("This email is not registered yet. Register first")
             return redirect('/registration')
-    return render_template("login.html", form=form)
+    return render_template("login.html", form=form, current_user=current_user)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect('/')
 
 
 @app.route("/registration", methods=["POST", "GET"])
@@ -94,23 +111,24 @@ def registration():
             flash("You've already signed up with that email, log in instead!")
             return redirect(url_for('login'))
 
-    return render_template("registration.html", form=form, logged_in=current_user.is_authenticated)
+    return render_template("registration.html", form=form, current_user=current_user)
 
 
 @app.route("/blog")
 def blog():
     all_blog = db.session.query(Blog).all()
-    return render_template("blog.html", blogs=all_blog, name=current_user, logged_in=True)
+    return render_template("blog.html", blogs=all_blog, current_user=current_user, logged_in=True)
 
 
 @app.route("/blog/<int:id>")
 def blog_details(id):
     individual_blog = Blog.query.get(id)
     blogs = db.session.query(Blog).all()
-    return render_template("blog_details.html", blog=individual_blog, blogs=blogs)
+    return render_template("blog_details.html", blog=individual_blog, blogs=blogs, current_user=current_user)
 
 
 @app.route("/post", methods=["POST", "GET"])
+@admin_only
 def post():
     form = PostBlog()
     if form.validate_on_submit():
@@ -122,10 +140,11 @@ def post():
         db.session.add(blog)
         db.session.commit()
         return redirect('/blog')
-    return render_template("post.html", form=form)
+    return render_template("post.html", form=form, current_user=current_user)
 
 
 @app.route("/update/<int:id>", methods=["POST", "GET"])
+@admin_only
 def update(id):
     blog_post = Blog.query.get(id)
     print(blog_post)
@@ -140,15 +159,16 @@ def update(id):
         blog_post.body = blog_record.body.data
         db.session.commit()
         return redirect("/blog")
-    return render_template("update.html", form=blog_record)
+    return render_template("update.html", form=blog_record, current_user=current_user)
 
 
 @app.route("/delete/<int:id>")
+@admin_only
 def delete(id):
     blog_to_delete = Blog.query.get(id)
     db.session.delete(blog_to_delete)
     db.session.commit()
-    return redirect("/blog")
+    return redirect("/blog", current_user=current_user)
 
 
 if __name__ == '__main__':
